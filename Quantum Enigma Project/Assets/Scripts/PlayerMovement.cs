739db4ed4785ed07ae17ee97b4341a475a1f14e6
Dominik <dmkk1@outlook.com>
2021-05-11 22:51:17 +0300

    EndGame Scene Attempt #1

--- Quantum Enigma Project/Assets/Scripts/PlayerMovement.cs
+++ Quantum Enigma Project/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -127,6 +128,11 @@ public class PlayerMovement : MonoBehaviour
 		{
             noteSound.Stop();
 		}
+
+        if(other.CompareTag("Chair"))
+		{
+            SceneManager.LoadScene("EndGame");
+        }
     }
 
 }
