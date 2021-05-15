9d6289c17cf555b72b4fa621a464f8151c06452f
Terboe <83028017+Terboe@users.noreply.github.com>
2021-05-15 17:34:20 +0300

    Counters

--- Quantum Enigma Project/Assets/Scripts/MiniGame1 Scripts/BoardManager.cs
+++ Quantum Enigma Project/Assets/Scripts/MiniGame1 Scripts/BoardManager.cs
@@ -42,6 +42,8 @@ public class BoardManager : MonoBehaviour
 
     private void Start()
     {
+        NumOfLevel.leveln = level_number;
+        LevelMovesLeft.movesl = 0 + BoardManager.Instance.moves_left;
         //Cursor.lockState = CursorLockMode.None;
         SpawnAllLevel();
     }
@@ -387,10 +389,12 @@ public static bool isPath(Marble[,] matrix,int fx,int fy, int i,
             }
             else
             {
+                LevelMovesLeft.movesl += 111;
                 level_number += 111;
                 SpawnAllLevel();
             }
         }
+        LevelMovesLeft.movesl = moves_left;
     }
 
     private void UpdateSelection()
@@ -473,6 +477,7 @@ public static bool isPath(Marble[,] matrix,int fx,int fy, int i,
         List<string> fileLines = File.ReadAllLines(readFromFilePath).ToList();
 
         moves_left = Int16.Parse(fileLines[0]);
+        LevelMovesLeft.movesl = moves_left;
 
         for (int i = 0; i < 8; ++i)
         {
