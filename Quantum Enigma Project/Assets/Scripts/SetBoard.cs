6cc7eeeb3c473469a133a0603a3765d564e66148
dawidwozny7 <68080414+dawidwozny7@users.noreply.github.com>
2021-05-24 12:10:18 +0300

    Door opens after finishing the levels in the room

--- Quantum Enigma Project/Assets/Scripts/SetBoard.cs
+++ Quantum Enigma Project/Assets/Scripts/SetBoard.cs
@@ -9,15 +9,23 @@ public class SetBoard : MonoBehaviour
     {
         if(ClearBoards.won1){
             GameObject.Find("Room1Board").SetActive(false);
+            GameObject.Find("Door/door_inner/Door Trigger").SetActive(true);
+            GameObject.Find("Door (1)/door_inner/Door Trigger").SetActive(true);
         }
         if(ClearBoards.won2){
             GameObject.Find("Room2Board").SetActive(false);
+            GameObject.Find("Door (2)/door_inner/Door Trigger").SetActive(true);
+            GameObject.Find("Door (3)/door_inner/Door Trigger").SetActive(true);
         }
         if(ClearBoards.won3){
             GameObject.Find("Room3Board").SetActive(false);
+            GameObject.Find("Door (4)/door_inner/Door Trigger").SetActive(true);
+            GameObject.Find("Door (5)/door_inner/Door Trigger").SetActive(true);
         }
         if(ClearBoards.won4){
             GameObject.Find("Room4Board").SetActive(false);
+            GameObject.Find("Door (6)/door_inner/Door Trigger").SetActive(true);
+            GameObject.Find("Door (7)/door_inner/Door Trigger").SetActive(true);
         }
         if(ClearBoards.won5){
             GameObject.Find("Room5Board").SetActive(false);
